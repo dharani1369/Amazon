357a3f41e3346ba366cbcdb74655c06bd486be0d
dharani1369 <dharanidhar.g@gmail.com>
2024-09-01 20:33:05 +0530

    Create App1.java

---
+++ Amazon-Core/src/main/java/org/amazon/App1.java
@@ -0,0 +1,14 @@
+package org.amazon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!dfdsfs" );
+        System.out.println( "Hello World!222" );
+    }
+}
