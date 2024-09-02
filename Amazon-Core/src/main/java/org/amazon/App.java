7372f3a12dde3ad70e5039a74d4e8f304ff9d3cf
dharani1369 <dharanidhar.g@gmail.com>
2024-09-02 23:23:56 +0530

    Update App.java

--- Amazon-Core/src/main/java/org/amazon/App.java
+++ Amazon-Core/src/main/java/org/amazon/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Dharani444ff!" );
+        System.out.println( "Hello Dharani4477ff!" );
     }
 }
